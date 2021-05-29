df8b71ba76decb94481d0c89357910256506794c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-05-29 17:37:46 +0200

    Prepare for 0.04 version

--- Detect.pm
+++ Detect.pm
@@ -12,7 +12,7 @@ use Readonly;
 # Constants.
 Readonly::Array our @EXPORT => qw(detect_dwg_file);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Detect DWG file.
 sub detect_dwg_file {
@@ -163,6 +163,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
